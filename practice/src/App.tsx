f7e4284b7ba8e141482ae2fd8eb19fd3a3b4fa23
Lee Ki-yoon <99546167+bubbletea03@users.noreply.github.com>
2023-09-19 11:23:46 +0000

    just feat the youtube player

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Youtube, {YouTubeProps} from 'react-youtube';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <Youtube videoId="2g811Eo7K8U"/>
       </header>
     </div>
   );
