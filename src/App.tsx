2397023d418c8e3c211b110e2678183ecf793089
Lee Ki-yoon <99546167+bubbletea03@users.noreply.github.com>
2023-09-21 11:33:48 +0000

    feat-ing autoplay system (but has NPE)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Youtube, {YouTubeProps} from 'react-youtube';
+import Youtube, {YouTubeEvent, YouTubePlayer, YouTubeProps} from 'react-youtube';
 
 function App() {
+
+  const [isPlayerSelected, setIsPlayerSelected] = useState(false);
+  const [player, setPlayer] = useState<YouTubePlayer>();
+
+  const onReady = (e: YouTubeEvent) => {
+    console.log("Ready!");
+    setIsPlayerSelected(true);
+    setPlayer(e.target);
+  };
+
+  const opts: YouTubeProps['opts'] = {
+    height: '300',
+    width: '500',
+    playerVars: {
+      controls: 0,
+    },
+  };
+
+  useEffect(() => {
+    if(isPlayerSelected) {
+      console.log("ㅇㅇ");
+    } 
+  }, [player]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +44,7 @@ function App() {
           Learn React
         </a>
 
-        <Youtube videoId="2g811Eo7K8U"/>
+        <Youtube videoId="2g811Eo7K8U" opts={opts} onReady={onReady}/>
       </header>
     </div>
   );
