b54bb00db794318785851e10a1b0c0db7f22dbec
Lee Ki-yoon <99546167+bubbletea03@users.noreply.github.com>
2023-09-27 11:29:00 +0000

    fixing autoplaying. later I will try react-player

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Youtube, {YouTubeEvent, YouTubePlayer, YouTubeProps} from 'react-youtube';
 
 function App() {
 
-  const [isPlayerSelected, setIsPlayerSelected] = useState(false);
-  const [player, setPlayer] = useState<YouTubePlayer>();
+  const refTest = useRef<HTMLDivElement>(null);
 
   const onReady = (e: YouTubeEvent) => {
-    console.log("Ready!");
-    setIsPlayerSelected(true);
-    setPlayer(e.target);
+    const iframe = refTest.current?.querySelector("iframe");
+    e.target.setVolume(0);
+    console.log(iframe);
+    setTimeout(() => {
+      iframe?.click();
+      console.log("왜안됨");
+    }, 2000);
   };
 
   const opts: YouTubeProps['opts'] = {
     height: '300',
     width: '500',
     playerVars: {
-      controls: 0,
+      autoplay: 1,
     },
   };
-
-  useEffect(() => {
-    if(isPlayerSelected) {
-      console.log("ㅇㅇ");
-    } 
-  }, [player]);
 
   return (
     <div className="App">
@@ -44,7 +41,9 @@ function App() {
           Learn React
         </a>
 
-        <Youtube videoId="2g811Eo7K8U" opts={opts} onReady={onReady}/>
+        <div ref={refTest}>
+          <Youtube videoId="2g811Eo7K8U" opts={opts} onReady={onReady} style={{pointerEvents:"none"}}/>
+        </div>
       </header>
     </div>
   );
