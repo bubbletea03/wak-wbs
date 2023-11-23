69b643fd20f78978642b427a22441ee6f0c61d17
bubbletea03 <99546167+bubbletea03@users.noreply.github.com>
2023-11-23 10:16:16 +0000

    feat: 오늘 방송 일정 없으면 테이블 안나오게 함

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import AllSchedule from "components/AllSchedule";
 import Player from "./components/Player";
 import ScheduleTable from "components/ScheduleTable";
 import styled from "styled-components";
 import VideoRecommends from "components/VideoRecommends";
+import { loadScheduleToday } from "schedule";
 
 function App() {
   /* TODO 스케쥴 잘못 입력하는 경우 예외처리 (방송 준비중 화면 띄우기)
     - from 혹은 to가 해당 비디오의 duration을 아예 넘었을 경우
     - 첫영상 기준 시각보다 일찍이거나, 영상 상영이 다 끝난 타이밍의 경우
   */
+  const scheduleToday = loadScheduleToday();
 
   return (
     <div className="App">
@@ -20,8 +21,18 @@ function App() {
         </DarkmodeButton>
 
         <Player />
-        <ScheduleTable />
-        <VideoRecommends />
+
+        {scheduleToday ? (
+          <>
+            <ScheduleTable />
+            <VideoRecommends />
+          </>
+        ) : (
+          <>
+            <div>오늘 방송 일정이 없습니다! 추천영상s:....</div>
+          </>
+        )}
+
         {/* <AllSchedule /> */}
       </header>
     </div>
